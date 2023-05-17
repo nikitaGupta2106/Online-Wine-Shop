ea059a17cee51bfe2f07fa3f604828088f907ac6
Kamlesh Bhatt <kb55262555@gmail.com>
2023-05-17 20:05:12 +0530

    JS added

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+let append = () =>{
+    let main = document.getElementById("options-append");
+    let rating = document.createElement("div");
+    rating.setAttribute("class", "rating");
+
+    let p = document.createElement("p");
+
+    let div = document.createElement("div");
+    div.setAttribute("class", "special-items");
+
+    let img = document.createElement("img");
+    img.src = "./Assets/great_deals_1-removebg-preview.png";
+    img.alt = "Great deals wine 1"
+
+
+}
